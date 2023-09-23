d30ce8ff148a8c203ea24eb4aae22dc1b7f6cc96
parutech <paruvitu@gmail.com>
2023-09-23 18:27:31 -0300

    create projetc

---
+++ App-Module-CocoaPods/App-Module-CocoaPods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App-Module-CocoaPods
+//
+//  Created by Paulo Pinheiro on 9/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
