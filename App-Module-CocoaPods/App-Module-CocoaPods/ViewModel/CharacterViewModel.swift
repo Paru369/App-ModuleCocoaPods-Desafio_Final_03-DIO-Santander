2714951a1a9e95a77a161bf82a673ec46cdbf1eb
parutech <paruvitu@gmail.com>
2023-09-23 19:52:52 -0300

    feat: ViewModel

--- App-Module-CocoaPods/App-Module-CocoaPods/ViewModel/CharacterViewModel.swift
+++ App-Module-CocoaPods/App-Module-CocoaPods/ViewModel/CharacterViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol CharacterViewModel: ObservableObject {
+    func getCharacters() async
+}
+
+@MainActor
+final class CharacterViewModelImpl: CharacterViewModel {
+    
+    enum State {
+        case na
+        case loading
+        case success(data: [Character])
+        case failed(error: Error)
+    }
+    
+    @Published private(set) var state: State = .na
+    @Published var hasError: Bool = false
+    
+    // Handle Errors with the alert
+    private let service: CharacterService
+    
+    init(service: CharacterService) {
+        self.service = service
+    }
+    
+   
+}
